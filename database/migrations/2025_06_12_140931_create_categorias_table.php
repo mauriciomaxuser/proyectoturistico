fed04bc38eb34d0b5664fd6a130862a8afee58bb
mauriciomaxuser <mariosaquinga026@gmail.com>
2025-06-12 09:11:56 -0500

    cambios en las migrciones de categorias

--- database/migrations/2025_06_12_140931_create_categorias_table.php
+++ database/migrations/2025_06_12_140931_create_categorias_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("nombre");
+
         });
     }
 
